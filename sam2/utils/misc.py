5922941e56977456d48e627c7f01eae41c744b46
Stephane Nguyen <stephane.nguyen@etu.unige.ch>
2025-04-03 18:14:09 +0200

    Clean inference in-place

--- sam2/utils/misc.py
+++ sam2/utils/misc.py
@@ -407,21 +407,20 @@ def concat_points(old_point_inputs, new_points, new_labels):
     return {"point_coords": points, "point_labels": labels}
 
 
-def clean_inference(inference_state, frames_to_keep=100):
+def clean_inference(inference_state: dict, frames_to_keep: int = 100) -> None:
     """
-    Clean at frequent intervals where we only keep the
+    Clean inference_state in-place at frequent intervals where we only keep the
     last frames_to_keep non-conditional frame outputs.
 
-    When not cleaning, the inference state is returned as is.
+    When not cleaning, the cleaning_counter key in the inference state is
+    incremented by one.
     """
+    assert np.all([key in inference_state for key in ["obj_ids", "cleaning_counter", "output_dict_per_obj"]])
+    old_obj_ids = inference_state["obj_ids"]
     if inference_state["cleaning_counter"] >= frames_to_keep:
         # Clean
         keys_to_remove = ["non_cond_frame_outputs"]
         for obj_idx in inference_state["output_dict_per_obj"]:
-            # print("Cleaning")
-            # print(
-            #     f'Before: {inference_state["output_dict_per_obj"][0]["non_cond_frame_outputs"].keys()}'
-            # )
             for key in keys_to_remove:
                 if key in inference_state["output_dict_per_obj"][obj_idx]:
                     frame_indices = sorted(
@@ -431,11 +430,8 @@ def clean_inference(inference_state, frames_to_keep=100):
                         inference_state["output_dict_per_obj"][obj_idx][key].pop(
                             frame_idx, None
                         )
-            # print(
-            #     f'After: {inference_state["output_dict_per_obj"][0]["non_cond_frame_outputs"].keys()}'
-            # )
         # Reset counter
         inference_state["cleaning_counter"] = 0
 
     inference_state["cleaning_counter"] += 1
-    return inference_state
+    assert inference_state["obj_ids"] == old_obj_ids, "Clean inference should not change the number of tracked objects"
